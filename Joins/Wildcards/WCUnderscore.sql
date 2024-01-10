de69c3c73549ded2244070e7819036f51d291935
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-10 09:24:51 +0530

    .

---
+++ Joins/Wildcards/WCUnderscore.sql
@@ -0,0 +1,3 @@
+select student_name from students where student_name like '__c%';
+select student_name from students where student_name like '_a%s';
+select student_name from students where student_name like '%n';
