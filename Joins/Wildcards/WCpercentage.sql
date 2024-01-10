0829d0cbd408b3e10aaf5b13627056331912f70d
Aravind Venkatachalam <95999211+Aravind8281@users.noreply.github.com>
2024-01-10 09:21:42 +0530

    .

---
+++ Joins/Wildcards/WCpercentage.sql
@@ -0,0 +1,3 @@
+select student_name from students where student_name like 'Lu%'; -- first two letters Lu
+select student_name from students where student_name like '%z'; -- lastletter
+select student_name from students where student_name like 'Mia%'; -- start with Mia
